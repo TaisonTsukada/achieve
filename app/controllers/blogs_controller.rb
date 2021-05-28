fd34b18c049d6d60ef93903b36ec7bac2ada510f
TaisonTsukada <78004609+TaisonTsukada@users.noreply.github.com>
2021-05-28 15:14:07 +0900

    CSSでデザインの調整

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -58,6 +58,11 @@ class BlogsController < ApplicationController
     end
   end
 
+  def home
+
+
+  end
+
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_blog
--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
   post  'inquiry/thanks'  => 'inquiry#thanks'
 
   mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
-  root to: 'blogs#index'
+  root to: 'blogs#home'
   resources :blogs
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
